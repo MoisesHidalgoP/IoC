f8eb62c790460cfd50b13072991806aab733cc29
DESKTOP-NISE1LI\Moises Hidalgo <csi21-mhidaza@colegioaltair.net>
2022-11-04 14:43:16 +0100

    Primer commit 04/11/2022

---
+++ IoC/src/main/java/com/app/ioc/Jeringa.java
@@ -0,0 +1,11 @@
+package com.app.ioc;
+
+public class Jeringa implements Utensilios {
+
+	public void usar() {
+		// TODO Auto-generated method stub
+		System.out.println("Usando Jeringa");
+		
+	}
+
+}
